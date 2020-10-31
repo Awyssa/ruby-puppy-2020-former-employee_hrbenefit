8fef13a087cfa078551a8ec316153cf82bbbe8b6
Michael Ernest <m.hendersun@gmail.com>
2020-10-31 13:43:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,3 +6,16 @@ class Dog
     @name = name
     @@all << self
   end
+
+  def self.all?
+    @all
+  end
+
+  def print_all 
+    puts @@all.map{ |dog| dog.name }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
